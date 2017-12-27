0e2a8391a6bb2fe492edec4fbad16eeecea6c49b
文 丁 <wuzao330389881@outlook.com>
2017-12-27 20:10:48 +0800

    SnakeAdd
    Siki贪吃蛇

---
+++ Snake/Assets/Scripts/MainUIController.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MainUIController : MonoBehaviour {
+
+    private static MainUIController _instance;
+    public static MainUIController Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    public int score = 0;
+    public int length = 0;
+    public Text msgText;
+    public Text scoreText;
+    public Text lengthText;
+    public Image bgImage;
+    private Color tempColor;
+    public Image pauseImage;
+    public Sprite[] pauseSprites;
+    public bool isPause = false;
+    public bool hasBorder = true;
+
+    void Awake()
+    {
+        _instance = this;
+    }
+
+    void Start()
+    {
+        if (PlayerPrefs.GetInt("border", 1) == 0)
+        {
+            hasBorder = false;
+            foreach (Transform t in bgImage.gameObject.transform)
+            {
+                t.gameObject.GetComponent<Image>().enabled = false;
+            }
+        }
+        else
+        {
+            hasBorder = true;
+        }
+    }
+
+    void Update()
+    {
+        switch (score / 200)
+        {
+            case 0:
+                break;
+            case 1:
+                ColorUtility.TryParseHtmlString("#CCEEFFFF", out tempColor);
+                bgImage.color = tempColor;
+                msgText.text = "阶段2";
+                bgImage.color = new Color();
+                break;
+            case 2:
+                ColorUtility.TryParseHtmlString("#CCEEFFFF", out tempColor);
+                bgImage.color = tempColor;
+                msgText.text = "阶段3";
+                bgImage.color = new Color();
+                break;
+            case 3:
+                ColorUtility.TryParseHtmlString("#EBFFDBFF", out tempColor);
+                bgImage.color = tempColor;
+                msgText.text = "阶段4";
+                bgImage.color = new Color();
+                break;
+            case 4:
+                ColorUtility.TryParseHtmlString("#FFF3CCFF", out tempColor);
+                bgImage.color = tempColor;
+                msgText.text = "阶段5";
+                bgImage.color = new Color();
+                break;
+            default:
+                ColorUtility.TryParseHtmlString("#CCDACCFF", out tempColor);
+                bgImage.color = tempColor;
+                msgText.text = "无尽阶段";
+                bgImage.color = new Color();
+                break;
+        }
+    }
+
+    public void UpdateUI(int s = 5, int l = 1)
+    {
+        score += s;
+        length += l;
+        scoreText.text = "得分:\n" + score;
+        lengthText.text = "长度:\n" + length;
+    }
+
+    public void Pause()
+    {
+        isPause = !isPause;
+        if (isPause)
+        {
+            Time.timeScale = 0;
+            pauseImage.sprite = pauseSprites[1];
+        }
+        else
+        {
+            Time.timeScale = 1;
+            pauseImage.sprite = pauseSprites[0];
+        }
+    }
+
+    public void Home()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+}
---
+++ Snake/Assets/Scripts/SnakeHead.cs
@@ -0,0 +1,179 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+//test github
+public class SnakeHead : MonoBehaviour {
+
+    public List<Transform> bodyList = new List<Transform>();
+    public float velocity = 0.35f;
+    public int step = 30;
+    private int x;
+    private int y;
+    private Vector3 headPos;
+    public GameObject bodyPrefab;
+    public GameObject dieEffect;
+    private bool isDie = false;
+    public AudioClip eatClip;
+    public AudioClip dieClip;
+
+    public Sprite[] bodySprites = new Sprite[2];
+    private Transform canvas;
+
+    void Awake()
+    {
+        canvas = GameObject.Find("Canvas").transform;
+        gameObject.GetComponent<Image>().sprite =
+            Resources.Load<Sprite>(PlayerPrefs.GetString("sh", "sh02"));
+        bodySprites[0] =
+            Resources.Load<Sprite>(PlayerPrefs.GetString("sb01", "sb0201"));
+        bodySprites[1] =
+            Resources.Load<Sprite>(PlayerPrefs.GetString("sb02", "sb0202"));
+    }
+    void Start()
+    {
+        InvokeRepeating("Move", 0, velocity);
+        x = 0;
+        y = step;
+    }
+
+    void Update()
+    {
+        if (!isDie)
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && !MainUIController.Instance.isPause)
+            {
+                CancelInvoke();
+                InvokeRepeating("Move", 0, velocity - 0.2f);
+            }
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                CancelInvoke();
+                InvokeRepeating("Move", 0, velocity);
+            }
+            if (Input.GetKey(KeyCode.W) && y != -step)
+            {
+                gameObject.transform.localRotation = Quaternion.Euler(0, 0, 0);
+                x = 0;
+                y = step;
+            }
+            else if (Input.GetKey(KeyCode.S) && y != step)
+            {
+                gameObject.transform.localRotation = Quaternion.Euler(0, 0, 180);
+                x = 0;
+                y = -step;
+            }
+            else if (Input.GetKey(KeyCode.A) && x != step)
+            {
+                gameObject.transform.localRotation = Quaternion.Euler(0, 0, 90);
+                x = -step;
+                y = 0;
+            }
+            else if (Input.GetKey(KeyCode.D) && x != -step)
+            {
+                gameObject.transform.localRotation = Quaternion.Euler(0, 0, -90);
+                x = step;
+                y = 0;
+            }
+        }
+    }
+    void Move()
+    {
+        headPos = gameObject.transform.localPosition;
+        gameObject.transform.localPosition = new Vector3(headPos.x + x, headPos.y + y);
+        if (bodyList.Count > 0)
+        {
+            //bodyList.Last().localPosition = headPos;
+            //bodyList.Insert(0, bodyList.Last());
+            //bodyList.RemoveAt(bodyList.Count - 1);
+            for (int i = bodyList.Count()-2; i >= 0; i--)
+            {
+                bodyList[i + 1].localPosition = bodyList[i].localPosition;
+            }
+            bodyList[0].localPosition = headPos;
+        }
+    }
+
+    void Grow()
+    {
+        AudioSource.PlayClipAtPoint(eatClip, Vector3.zero);
+        GameObject body = Instantiate(bodyPrefab, new Vector3(2000, 2000, 0), Quaternion.identity);
+        body.GetComponent<Image>().sprite = bodySprites[bodyList.Count % 2];
+        body.transform.SetParent(canvas, false);
+        bodyList.Add(body.transform);
+    }
+
+    void Die()
+    {
+        AudioSource.PlayClipAtPoint(dieClip, Vector3.zero);
+        CancelInvoke();
+        isDie = true;
+        Instantiate(dieEffect);
+        PlayerPrefs.SetInt("lastl", MainUIController.Instance.length);
+        PlayerPrefs.SetInt("lasts", MainUIController.Instance.score);
+        if (PlayerPrefs.GetInt("bests", 0) < MainUIController.Instance.score)
+        {
+            PlayerPrefs.SetInt("bestl", MainUIController.Instance.length);
+            PlayerPrefs.SetInt("bests", MainUIController.Instance.score);
+        }
+        StartCoroutine(GameOver(1.5f));
+    }
+
+    IEnumerator GameOver(float t)
+    {
+        yield return new WaitForSeconds(t);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Food"))
+        {
+            Destroy(collision.gameObject);
+            MainUIController.Instance.UpdateUI();
+            Grow();
+            FoodMaker.Instance.MakeFood(Random.Range(0, 100) < 20);
+        }
+        else if (collision.gameObject.CompareTag("Reward"))
+        {
+            Destroy(collision.gameObject);
+            MainUIController.Instance.UpdateUI(Random.Range(5,15)*10);
+            Grow();
+        }
+        else if (collision.gameObject.CompareTag("Body"))
+        {
+            Die();
+        }
+        else
+        {
+            if (MainUIController.Instance.hasBorder)
+            {
+                Die();
+            }
+            else
+            {
+                switch (collision.gameObject.name)
+                {
+                    case "Up":
+                        transform.localPosition = new Vector3(transform.localPosition.x,
+                            -transform.localPosition.y + 30, transform.localPosition.z);
+                        break;
+                    case "Down":
+                        transform.localPosition = new Vector3(transform.localPosition.x,
+                            -transform.localPosition.y - 30, transform.localPosition.z);
+                        break;
+                    case "Left":
+                        transform.localPosition = new Vector3(-transform.localPosition.x + 180,
+                            transform.localPosition.y, transform.localPosition.z);
+                        break;
+                    case "Right":
+                        transform.localPosition = new Vector3(-transform.localPosition.x + 240,
+                            transform.localPosition.y + 30, transform.localPosition.z);
+                        break;
+                }
+            }
+        }
+    }
+}
